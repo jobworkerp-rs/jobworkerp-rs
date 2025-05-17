9392dda1b9f10469e55886fdd8f4429120ee3812
sutr-app <91235033+sutr-app@users.noreply.github.com>
2025-05-17 15:59:22 +0900

    fix sql

--- infra/sql/mysql/003_function.sql
+++ infra/sql/mysql/003_function.sql
@@ -1,6 +1,6 @@
 
 -- function set definition
-DROP TABLE IF EXISTS function function_set;
+DROP TABLE IF EXISTS function_set;
 CREATE TABLE `function_set` (
   `id` BIGINT(10) PRIMARY KEY,
   `name` VARCHAR(128) NOT NULL, -- name for identification
@@ -10,13 +10,12 @@ CREATE TABLE `function_set` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- function set target definition
-DROP TABLE IF EXISTS function function_set_target;
+DROP TABLE IF EXISTS function_set_target;
 CREATE TABLE `function_set_target` (
   `id` BIGINT(10) PRIMARY KEY AUTO_INCREMENT,
   `set_id` BIGINT(10) NOT NULL, -- function set id
   `target_id` BIGINT(10) NOT NULL, -- function set target id(worker or runner)
   `target_type` INT(10) NOT NULL DEFAULT 0, -- function set target type (runner: 0 or worker: 1)
-  UNIQUE KEY `set_target` (`set_id`, `target_id`, `target_type`),
+  UNIQUE KEY `set_target` (`set_id`, `target_id`, `target_type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 
