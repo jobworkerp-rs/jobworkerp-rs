b6e0905ebdc75cc786b603d4bab7eaf86d04f1b2
sutr-app <91235033+sutr-app@users.noreply.github.com>
2025-12-21 12:13:10 +0900

    fix

--- docker/start-all-in-one.sh
+++ docker/start-all-in-one.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
+# Exit code to use when shutting down
+FINAL_EXIT_CODE=0
+
 # Cleanup function to stop nginx on exit
 cleanup() {
+    local exit_code=${1:-$FINAL_EXIT_CODE}
     echo "Stopping nginx..."
     nginx -s quit
-    exit 0
+    exit "$exit_code"
 }
 
-# Trap SIGTERM and SIGINT to cleanup
-trap cleanup SIGTERM SIGINT
+# Trap SIGTERM and SIGINT to cleanup with appropriate exit code
+trap 'cleanup 143' SIGTERM  # 128 + 15 (SIGTERM)
+trap 'cleanup 130' SIGINT   # 128 + 2 (SIGINT)
 
 # Generate runtime config for admin-ui
 /home/jobworkerp/env.sh
@@ -17,15 +22,12 @@ trap cleanup SIGTERM SIGINT
 nginx
 
 # Start all-in-one as jobworkerp user (foreground)
-# Use exec to replace shell process, but we need to keep trap working
 gosu jobworkerp ./all-in-one &
 ALL_IN_ONE_PID=$!
 
 # Wait for all-in-one to exit
 wait $ALL_IN_ONE_PID
-EXIT_CODE=$?
+FINAL_EXIT_CODE=$?
 
 # Cleanup nginx when all-in-one exits
-cleanup
-
-exit $EXIT_CODE
+cleanup "$FINAL_EXIT_CODE"
