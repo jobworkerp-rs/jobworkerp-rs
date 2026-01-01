56b44f4356ab079e9be12690fca23efbcc59e7ed
sutr-app <91235033+sutr-app@users.noreply.github.com>
2026-01-02 07:43:49 +0900

    fix worker instance registration and cleanup in not all-in-one env

--- grpc-front/src/bin/front.rs
+++ grpc-front/src/bin/front.rs
@@ -1,9 +1,11 @@
 use std::sync::Arc;
 
 use anyhow::Result;
+use app::app::worker_instance::InstanceCleanupTask;
 use app::module::{AppConfigModule, AppModule};
 use command_utils::util::shutdown;
 use dotenvy::dotenv;
+use infra::infra::worker_instance::UseWorkerInstanceRepository;
 use jobworkerp_runner::runner::{
     factory::RunnerSpecFactory,
     mcp::{config::McpConfig, proxy::McpServerFactory},
@@ -16,6 +18,9 @@ async fn main() -> Result<()> {
     dotenv().ok();
 
     command_utils::util::tracing::init_from_env_and_filename("jobworkerp-front", "log").await?;
+
+    // Create shutdown signal for coordinated shutdown
+    let (shutdown_send, shutdown_recv) = tokio::sync::watch::channel(false);
 
     // load mcp config
     let mcp_clients = match McpConfig::load(&jobworkerp_base::MCP_CONFIG_PATH.clone()).await {
@@ -34,17 +39,42 @@ async fn main() -> Result<()> {
         mcp_clients,
     ));
     let config_module = Arc::new(AppConfigModule::new_by_env(runner_factory));
-    let app_module = Arc::new(AppModule::new_by_env(config_module).await?);
+    let app_module = Arc::new(AppModule::new_by_env(config_module.clone()).await?);
     // setup runner on start front
     app_module.on_start_front().await?;
 
+    // Start instance cleanup task (grpc-front only: cleanup expired instances)
+    let storage_type = config_module.storage_type();
+    let repository = app_module.repositories.worker_instance_repository();
+    let cleanup_task = InstanceCleanupTask::new(repository, storage_type);
+    let cleanup_handle =
+        tokio::spawn(async move { cleanup_task.start_cleanup_loop(shutdown_recv).await });
+
     let (lock, mut wait) = shutdown::create_lock_and_wait();
+
+    // Spawn ctrl_c handler that broadcasts shutdown signal
+    let ctrl_c_shutdown_send = shutdown_send.clone();
+    tokio::spawn(async move {
+        match tokio::signal::ctrl_c().await {
+            Ok(()) => {
+                tracing::info!("received ctrl_c, sending shutdown signal");
+                let _ = ctrl_c_shutdown_send.send(true);
+            }
+            Err(e) => tracing::error!("failed to listen for ctrl_c: {:?}", e),
+        }
+    });
 
     // trace::tracing_init_jaeger(&jeager_addr);
     let ret = grpc_front::start_front_server(app_module, lock).await;
 
     tracing::info!("waiting shutdown");
     wait.wait().await;
+
+    // Send shutdown signal (in case not already sent)
+    let _ = shutdown_send.send(true);
+
+    // Wait for cleanup task to finish
+    let _ = cleanup_handle.await;
 
     command_utils::util::tracing::shutdown_tracer_provider();
     tracing::info!("shutdown");
